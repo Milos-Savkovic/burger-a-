841584d718620498cceeea3bad25b32c67ccc37b
Milos Savkovic <milos.savkovic@jsguru.io>
2018-11-19 14:53:55 +0100

    Adding the Order button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,13 +13,20 @@ const controls = [
 const buildControls = props => {
   return (
     <div className="BuildControls">
+      <p>
+        Current price: <strong>{props.price.toFixed(2)}</strong>
+      </p>
       {controls.map(ctrl => (
         <BuildControl
           key={ctrl.label}
           label={ctrl.label}
-          added={props.ingredientAdded}
+          type={ctrl.type}
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
         />
       ))}
+      <button className="OrderButton">ORDER NOW</button>
     </div>
   );
 };
